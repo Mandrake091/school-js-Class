3b2337f750f73a5ee69297be30d26a0721d3fb71
Tails <gianma.bennici@gmail.com>
2022-12-01 12:35:20 +0100

    added validate functions

--- src/baseClass.js
+++ src/baseClass.js
@@ -8,30 +8,18 @@ class BaseClass {
     }
 
     validate() {
-        console.log(`Validate: `, this);
-        let validationResult = {
-          invalidAttributes: [],
-        };
-        try {
-          for (let attribute in this.attributes) {
-            console.log(
-              `Validating ${attribute} of type ${this.attributes[attribute].type} for value ${this[attribute]}`
-            );
-            const attributeIsValid = this.attributes[attribute].validate(
-              this[attribute]
-            );
-            if (!attributeIsValid) {
-              validationResult.invalidAttributes.push(attribute);
-            }
-          }
-          validationResult.success =
-            validationResult.invalidAttributes.length === 0;
-        } catch (err) {
-          console.log(`Error: `, err);
-          validationResult.success = false;
+      let validationResult = {
+        invalidAttributes: [],
+      };
+      for (let attribute in this.attributes) {
+        const attributeIsValid = this.attributes[attribute].validate(this[attribute]);
+        if (!attributeIsValid) {
+          validationResult.invalidAttributes.push(attribute);
         }
-        return validationResult;
       }
+      validationResult.success = validationResult.invalidAttributes.length === 0;
+      return validationResult;
+    }
 }
 
 export {BaseClass};
